dfc59ebb87e547d822ab8e5c0575842a9492adba
Miguel Rodríguez González <uo263494@uniovi.es>
2023-12-02 23:03:40 +0100

    feat: improve style

--- src/1_day/main.ts
+++ src/1_day/main.ts
@@ -1,49 +1,49 @@
 const testCaseOne = [
-  {
-    gift: [2, 1, 3, 5, 3, 2],
-    expected: 3,
-  },
-  {
-    gift: [1, 2, 3, 4],
-    expected: -1,
-  },
-  {
-    gift: [5, 1, 5, 1],
-    expected: 5,
-  },
-  {
-    gift: [1, 5, 5, 1],
-    expected: 5,
-  },
-  {
-    gift: [2, 1, 3, 5, 3, 2, 6],
-    expected: 3,
-  },
+    {
+        gift: [2, 1, 3, 5, 3, 2],
+        expected: 3
+    },
+    {
+        gift: [1, 2, 3, 4],
+        expected: -1
+    },
+    {
+        gift: [5, 1, 5, 1],
+        expected: 5
+    },
+    {
+        gift: [1, 5, 5, 1],
+        expected: 5
+    },
+    {
+        gift: [2, 1, 3, 5, 3, 2, 6],
+        expected: 3
+    }
 ];
 
-const findFirstRepeated = (gift: Array<number>) => {
-  const set = new Set(gift);
-  // no hay repetidos
-  if (set.size === gift.length) return -1;
+const findFirstRepeated = (gifts: Array<number>) => {
+    const set = new Set(gifts);
+    // no hay repetidos
+    if (set.size === gifts.length) return -1;
 
-  // hay repetidos
-  const perfectArray = Array.from(set.values());
+    // hay repetidos
+    const perfectArray = Array.from(set.values());
 
-  for (let i = 0; i < gift.length; i++) {
-    const value = gift.at(i);
-    if (value !== perfectArray.at(i)) {
-      return value;
+    for (let i = 0; i < gifts.length; i++) {
+        const value = gifts.at(i);
+        if (value !== perfectArray.at(i)) {
+            return value;
+        }
     }
-  }
-  return -1;
+    return -1;
 };
 
 testCaseOne.forEach(({ gift, expected }, index) => {
-  const start = performance.now();
-  console.log(
-    `${gift} 
+    const start = performance.now();
+    console.log(
+        `${gift} 
     -> expected: ${expected} -> received ${findFirstRepeated(gift)}`
-  );
-  const end = performance.now();
-  console.log(`Case ${index + 1} -> took ${end - start} milliseconds\n`);
+    );
+    const end = performance.now();
+    console.log(`Case ${index + 1} -> took ${end - start} milliseconds\n`);
 });
--- src/2_day/main.ts
+++ src/2_day/main.ts
@@ -1,39 +1,39 @@
 const testCaseTwo = [
-  {
-    gifts: ["tren", "oso", "pelota"],
-    materials: "tronesa", // tren oso
-    expected: ["tren", "oso"],
-  },
-  {
-    gifts: ["juego", "puzzle"],
-    materials: "jlepuz",
-    expected: ["puzzle"],
-  },
-  {
-    gifts: ["libro", "ps5"],
-    materials: "psli",
-    expected: [],
-  },
+    {
+        gifts: ['tren', 'oso', 'pelota'],
+        materials: 'tronesa', // tren oso
+        expected: ['tren', 'oso']
+    },
+    {
+        gifts: ['juego', 'puzzle'],
+        materials: 'jlepuz',
+        expected: ['puzzle']
+    },
+    {
+        gifts: ['libro', 'ps5'],
+        materials: 'psli',
+        expected: []
+    }
 ];
 
 const manufacture = (gifts: Array<string>, materials: string) => {
-  let values = new Array<string>();
-  gifts.forEach((value) => {
-    const gift = Array.from(new Set(value).values());
-    if (gift.every((v) => materials.indexOf(v) !== -1)) {
-      // se puede hacer el gift
-      values.push(value);
-    }
-  });
+    let values = new Array<string>();
+    gifts.forEach((value: string) => {
+        const gift = Array.from(new Set(value).values());
+        if (gift.every((v) => materials.indexOf(v) !== -1)) {
+            // se puede hacer el gift
+            values.push(value);
+        }
+    });
 
-  return values;
+    return values;
 };
 
 testCaseTwo.forEach(({ gifts, materials, expected }) =>
-  console.log(`
+    console.log(`
         gifts: [${gifts}]  materials: [${materials}]
         expected: [${expected}] received:  [${manufacture(
-    gifts,
-    materials
-  )}]\n `)
+        gifts,
+        materials
+    )}]\n `)
 );
