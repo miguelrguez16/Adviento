246450038ac4a25119281f4c289cc3dd19831b3a
Miguel Rodríguez González <uo263494@uniovi.es>
2023-12-12 22:21:39 +0100

    aps

--- src/5_day/main.ts
+++ src/5_day/main.ts
@@ -4,7 +4,7 @@ function cyberReindeer(road: string, time: number) {
     for (let i = 1; i < time; i++) {}
     return log;
 }
-  
+
 const road = 'S..|...|..';
 const time = 10; // unidades de tiempo
 const result = cyberReindeer(road, time);
